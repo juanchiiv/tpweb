0b11a72ef7a1d88c48433a65adf80ed18be401d5
Lautaro Lamenza <laulamenza22@live.com.ar>
2022-07-12 16:58:49 -0300

    actualizo base de datos

--- db/ahs.sql
+++ db/ahs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-06-2022 a las 00:57:50
+-- Tiempo de generación: 12-07-2022 a las 21:57:32
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -35,6 +35,15 @@ CREATE TABLE `comentarios` (
   `puntuacion` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `comentarios`
+--
+
+INSERT INTO `comentarios` (`id`, `id_episodio`, `id_usuario`, `comentario`, `puntuacion`) VALUES
+(1, 1, 1, 'xmvbncmbvc', 4),
+(5, 1, 5, 'hfdhdhfdhf', 5),
+(6, 1, 5, 'vdfdffdbfdbd', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -54,13 +63,14 @@ CREATE TABLE `episodios` (
 --
 
 INSERT INTO `episodios` (`id_episodios`, `nombre`, `descripcion`, `audiencia`, `id_temporada_FK`) VALUES
-(1, 'piloto', 'whbbjwfhbwfejwefhbwfjwbf', 3.18, 1),
-(2, 'home invasion', 'hhrwgfhjfwehfjefhw', 2.46, 1),
-(3, 'welcome to briarcliff', 'hhrwgfhjfwehfjefhw', 3.85, 2),
-(8, 'Boy parts', 'hjsgfhjd', 2.5, 5),
+(1, 'Piloto', 'La familia Harmon - Ben (Dylan McDermott), Vivien (Connie Britton) y Violet (Taissa Farmiga) - se mudan de Boston a Los Ángeles después que Vivien tiene un aborto involuntario y Ben tiene una aventura con una de sus estudiantes. La familia se muda a una mansión restaurada, sin saber que está encantada por sus expropietarios. Cuando Vivien intenta hacer frente a su vecina instrusa, Constance (Jessica Lange), Violet se conecta con el adolescente Tate (Evan Peters).', 3.18, 1),
+(2, 'Home invasion', 'Ben Harmon (Dylan McDermott) se dirige a Boston para hablar con la estudiante que tuvo la aventura en el primer episodio (la estrella invitada Kate Mara). Mientras está ausente, su esposa, Vivien (Connie Britton), y su hija, Violet (Taissa Farmiga), lidian con tres invasores quiénes quieren volver a representar un asesinato que ocurrió en la casa en 1968.', 2.46, 1),
+(3, 'Welcome to briarcliff', 'El episodio introduce al elenco principal, y narra el intento de la reportera Lana Winters (Sarah Paulson) de denunciar los tratos sadísticos de la Hermana Jude (Jessica Lange) en su hospital psiquiátrico, así como el injusto confinamiento de Kit Walker (Evan Peters) en la institución.', 3.85, 2),
+(8, 'Bitchcraft', 'El episodio presenta un grupo de cuatro jóvenes brujas, las cuales asisten a una escuela en New Orleans donde aprenderán a controlar sus poderes. Al mismo tiempo, se cuenta la historia de Madame Delphine LaLaurie, una cruel dama de sociedad del siglo XIX que mutilaba y torturaba a sus esclavos.', 5.54, 3),
 (9, 'Boy parts', 'Aún tratando de seguir siendo joven y hermosa, Fiona interroga a su nueva rehén: Madame Delphine LaLaurie. Delphine cuenta que hace 180 años atrás una sacerdotisa vudú llamada Marie Laveau le dio un elixir inmortal, mató a su familia, y la enterró viva. Con la esperanza de volverse inmortal, Fiona visita a Laveau, quien aún vive y trabaja en un salón de belleza. Fiona y Laveau crean una nueva rivalidad entre las brujas de Salem y los practicantes de vudú. Mientras tanto, Zoe y Madison deciden lanzar un hechizo de resurrección con el fin de traer de vuelta a Kyle a la vida, pero Kyle regresa como un monstruo asustado, enojado y solitario. En la escuela, Cordelia y su marido, Hank, tienen problemas para concebir a un niño, por lo que realizan un ritual de fertilidad.', 4.51, 3),
-(13, 'lkshfkdshf', 'dsfhfds', 232, 2),
-(14, 'Boy parts', 'kjdkjasnklsakl', 4.51, 6);
+(13, 'Monster among us', 'El episodio nos transporta al año 1952 y nos presenta a Elsa Mars (Jessica Lange), quien dirige uno de los últimos espectáculos de fenómenos (llamados freaks) de los Estados Unidos en la aldea de Júpiter, Florida, y también el reclutamiento, por parte de Elsa, de las gemelas unidas Bette y Dot Tattler (Sarah Paulson). También presenta al resto de sus artistas del espectáculo de freaks y al payaso asesino conocido como Twisty (John Carroll Lynch). El episodio es el segundo más largo en la historia de la serie, llegando a poco más de una hora de duración sin comerciales. Este episodio es notable en la serie, ya que es el primer episodio de estreno de una temporada que no presenta flashbacks a otro año.', 6.13, 4),
+(14, 'Massacres and Matinees', 'En este episodio, se impone un toque de queda en Júpiter, mientras la policía investiga el Espectáculo de Freaks cuando sospechan que un policía fue asesinado en el lugar.', 4.53, 4),
+(25, 'Checking in', 'Unas turistas suecas sufren un repentino ataque producido por una criatura extraña que aparece en su habitación del hotel donde se alojan. Iris, la gerente del centro, decide trasladarlas de departamento pero su intención queda anulada por la presencia de otras criaturas cuyo origen es desconocido. Así, se van sucediendo una serie de catástrofes imposibles de parar a mano de unos personajes totalmente peculiares. Entre ellos, la dueña del edificio, la condesa Elizabeth, quien tiene por costumbre llevar a cabo prácticas fuera de lo común.', 5.81, 5);
 
 -- --------------------------------------------------------
 
@@ -82,7 +92,8 @@ CREATE TABLE `login` (
 
 INSERT INTO `login` (`id_usuario`, `nombre`, `password`, `email`, `rol`) VALUES
 (1, 'lautaro', '$2y$10$5bsZ6NLEQ7LPDO.hvVzyUeDAiPbB.pQOIWL4qauo2OLj5WxHmhmVW', 'laulamenza22@live.com.ar', 'admin'),
-(2, 'juanchi', '$2y$10$dbiO57Eq8bSSENTqY2F3zOx1wHwGvHKTLhOmy/Q/yHgCU73FqXTf.', 'juanchiilobe@gmail.com', 'admin');
+(2, 'juanchi', '$2y$10$dbiO57Eq8bSSENTqY2F3zOx1wHwGvHKTLhOmy/Q/yHgCU73FqXTf.', 'juanchiilobe@gmail.com', 'admin'),
+(5, 'Martin', '$2y$10$R2yVIRoI14AvtMc.pFWOCeADV.mbRXDwXM4puTIfuwTHYBa0KQD/i', 'martin@gmail.com', 'usuario');
 
 -- --------------------------------------------------------
 
@@ -105,7 +116,7 @@ INSERT INTO `temporada` (`id_temporada`, `nombre_temporada`) VALUES
 (3, 'Coven'),
 (4, 'Freak Show'),
 (5, ' Hotel'),
-(6, 'kaklsfla');
+(6, 'Roanoke');
 
 --
 -- Índices para tablas volcadas
@@ -146,25 +157,25 @@ ALTER TABLE `temporada`
 -- AUTO_INCREMENT de la tabla `comentarios`
 --
 ALTER TABLE `comentarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `episodios`
 --
 ALTER TABLE `episodios`
-  MODIFY `id_episodios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_episodios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `login`
 --
 ALTER TABLE `login`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `temporada`
 --
 ALTER TABLE `temporada`
-  MODIFY `id_temporada` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_temporada` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restricciones para tablas volcadas
